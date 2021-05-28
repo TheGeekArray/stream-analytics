a9cf032f9a829073aa76031b8aed7ed2a541c284
CaveMobster <cavemobster@gmail.com>
2021-05-29 00:05:02 +0200

    Build analytics tool

---
+++ src/components/Bar.vue
@@ -0,0 +1,22 @@
+<script>
+import { Bar } from 'vue-chartjs'
+
+export default {
+  extends: Bar,
+  props: ["data", "options", "label"],
+  mounted () {
+    const chartdata = {
+      labels: this.data["Date"],
+      datasets: [
+        {
+          label: this.label,
+          backgroundColor: "#772ce8",
+          data: this.data[this.label],
+        }
+      ]
+    };
+
+    this.renderChart(chartdata, this.options);
+  }
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,130 @@
+<template>
+  <div class="home">
+    <div class="header">
+      <FileReader @load="parseData"/>
+      <Dropdown v-if="loaded" @change="updateLabel" v-bind:labels="allLabels" :key="dropdownKey"/>
+    </div>
+    <div class="bar-container">
+      <AverageViewers v-if="label === 'Average Viewers' && loaded" v-bind:data="chartdata" v-bind:options="options" v-bind:label="label" :key="barKey"/>
+      <Bar v-if="label !== 'Average Viewers'  && loaded" v-bind:data="chartdata" v-bind:options="options" v-bind:label="label" :key="barKey"/>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Bar from '@/components/Bar.vue'
+import FileReader from '@/components/FileReader.vue'
+import Dropdown from '@/components/Dropdown.vue'
+import AverageViewers from '@/components/AverageViewers.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Bar,
+    FileReader,
+    Dropdown,
+    AverageViewers
+  },
+  data: () => (
+    { 
+      loaded: false, 
+      data: "",
+      options: "",
+      chartdata: "",
+      allLabels: "",
+      label: "Average Viewers", 
+      barKey: 0,
+      dropdownKey: 0
+    }
+  ),
+  methods: {
+    updateLabel: function (event) {
+      this.label = event;
+      this.barKey++;
+    },
+    parseData: function(data) {
+      const splittedData = this.splitData(data);
+
+      this.chartdata = this.mapData(splittedData);
+      this.allLabels = Object.keys(this.chartdata).filter(topic => topic !== "Date");
+      this.options = {
+        responsive: true,
+        maintainAspectRatio: false,
+        tooltips: {
+          mode: "x",
+          callbacks: {
+            label: function(tooltipItem, data) {
+              var label = data.datasets[tooltipItem.datasetIndex].label || '';
+
+              if (label) {
+                  label += ': ';
+              }
+              label += (tooltipItem.yLabel % 1 !== 0 ? tooltipItem.yLabel.toFixed(2) : tooltipItem.yLabel);
+              return label;
+            }
+          }
+        },
+        scales: {
+          scaleLabel: {
+            fontColor: "#FF0000"
+          },
+          xAxes: [
+            {
+              stacked: true,
+            }
+          ],
+          yAxes: [
+            {
+              stacked: true
+            }
+          ]
+        }
+      };
+
+      this.barKey++;
+      this.loaded = true;
+    },
+    splitData: function (data) {
+      const dataToSplit = data.split("\n");
+      const splittedData = [];
+      
+      for (let line of dataToSplit) {
+          let splittedLine = line.split(",");
+          splittedData.push(splittedLine);
+        }
+
+      return splittedData;
+    },
+    mapData: function (data) {
+      const topics = data.shift();
+      const twitchData = {};
+
+      let count = 0;
+      for (let topic of topics) {
+        twitchData[topic] = [];
+        
+        for (let line of data) {
+          twitchData[topic].push(line[count]);
+        }
+        
+        count++;
+      }
+
+      return twitchData;
+    }
+  }
+}
+</script>
+
+<style>
+  .home {
+    width: 90%;
+    margin: 0 auto;
+  }
+
+  .header {
+    display: flex;
+    justify-content: space-between;
+  }
+</style>
