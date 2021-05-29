852da92e397d0fb2d6b108465c478ade794c3adc
CaveMobster <cavemobster@gmail.com>
2021-05-29 09:36:30 +0200

    Add a chartdata getter method

--- src/components/Bar.vue
+++ src/components/Bar.vue
@@ -5,18 +5,23 @@ export default {
   extends: Bar,
   props: ["data", "options", "label"],
   mounted () {
-    const chartdata = {
-      labels: this.data["Date"],
-      datasets: [
-        {
-          label: this.label,
-          backgroundColor: "#772ce8",
-          data: this.data[this.label],
-        }
-      ]
-    };
-
+    const chartdata = this.getChartData();
+    
     this.renderChart(chartdata, this.options);
+  },
+  methods: {
+    getChartData: function() {
+      return chartdata = {
+        labels: this.data["Date"],
+        datasets: [
+          {
+            label: this.label,
+            backgroundColor: "#772ce8",
+            data: this.data[this.label]
+          }
+        ]
+      };
+    }
   }
 }
 </script>
