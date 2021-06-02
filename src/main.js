488df2a2af969116cfecb8048dd1738adc4f87a3
CaveMobster <cavemobster@gmail.com>
2021-06-02 12:07:20 +0200

    Add the cog and reply icon

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faCog, faReply } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+
+library.add(faCog);
+library.add(faReply);
+
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 Vue.config.productionTip = false;
 
