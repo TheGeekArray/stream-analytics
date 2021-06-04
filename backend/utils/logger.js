80a729bd7b7e8fad55386b1c4c4d1f5b9028f7e9
CaveMobster <cavemobster@gmail.com>
2021-06-04 17:55:35 +0200

    Add a custom logger

---
+++ backend/utils/logger.js
@@ -0,0 +1,35 @@
+// "use strict";
+
+// https://en.wikipedia.org/wiki/ANSI_escape_code
+
+export default class Logger {
+	static info(message) {
+		const infoLabel = '\x1b[30;104m SA INFO \x1b[0m';
+		console.info(infoLabel, `${message}`);
+	}
+
+	static success(message) {
+		const infoLabel = '\x1b[30;102m SA SUCCESS \x1b[0m';
+		console.info(infoLabel, `${message}`);
+	}
+
+	static error(message, trace = false) {
+		const infoLabel = '\x1b[30;101m SA ERROR \x1b[0m';
+
+		console.error(infoLabel, `${message}`);
+		
+		if(trace) {
+			console.trace();
+		}
+	}
+
+	static debug(message, trace = false) {
+		const infoLabel = '\x1b[30;45m SA DEBUG \x1b[0m';
+
+		console.info(infoLabel, `${message}`);
+
+		if(trace) {
+			console.trace();
+		}
+	}
+}
