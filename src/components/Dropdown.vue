ad631467efd258b2ee45f5a50ed93f14d39b7fee
CaveMobster <cavemobster@gmail.com>
2021-06-05 12:49:09 +0200

    Add a dropdown to choose time units

---
+++ src/components/Dropdown.vue
@@ -0,0 +1,21 @@
+<template>
+	<div class="time-unit-dropdown">
+		<select id="time-unit-dropdown-select" name="time-units" @change="getValue" v-model="selected">
+			<option v-for="(unit, index) in timeUnits" :key="index" :value="unit">{{unit}}</option>
+		</select>
+	</div>
+</template>
+
+<script>
+	export default {
+		data: () => ({ 
+			selected: "Day",
+			timeUnits: ["Day", "Week", "Month", "Year"]
+		}),
+		methods: {
+			getValue(event) {
+				this.$emit("change", event.target.value);
+			}
+		}
+	}
+</script>
