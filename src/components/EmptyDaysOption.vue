5d25b1ad9f0b1171ab038259ba56564eeafdf059
CaveMobster <cavemobster@gmail.com>
2021-06-08 10:30:38 +0200

    Change the unit display based on which stats display is chosen

--- src/components/EmptyDaysOption.vue
+++ src/components/EmptyDaysOption.vue
@@ -1,11 +1,41 @@
 <template>
 	<div class="hide-empty-days-option">
-		<input type="checkbox" :checked="isChecked" @change="$emit('change', $event.target.checked)"> Hide non-stream days
+		<input type="checkbox" :checked="isChecked" @change="$emit('change', $event.target.checked)"> Hide non-stream {{unit}}
 	</div>
 </template>
 
 <script>
 export default {
-	props: { isChecked: Boolean }
+	props: { 
+		isChecked: Boolean,
+		timeUnit: String
+	},
+	data: function() {
+		return {
+			unit: "days"
+		}
+	},
+	watch: {
+		timeUnit: function(value) {
+			switch(value) {
+				case "30 days":
+				case "Day":
+					this.unit = "days";
+					break;
+				case "Week":
+					this.unit = "weeks";
+					break;
+				case "Month":
+					this.unit = "months";
+					break;
+				case "Year":
+					this.unit = "years";
+					break;
+				default:
+					this.unit = "time";
+					break;
+			}
+		}
+	}
 }
 </script>
