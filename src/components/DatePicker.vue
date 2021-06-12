32daabd96d9a5f634dca5ffc5ff9666c36154a23
CaveMobster <cavemobster@gmail.com>
2021-06-12 14:21:59 +0200

    Add a default range of 30 days

--- src/components/DatePicker.vue
+++ src/components/DatePicker.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="date-picker">
-		<input type="date" id="start-date" :max="range.end || today" v-model="range.start" @change="changeRange();">
-		<input type="date" id="end-date" :min="range.start" :max="today" v-model="range.end" @change="changeRange();">
+		<input type="date" id="start-date" :max="range.end || today" v-model="range.start" @change="$emit('change', range);">
+		<input type="date" id="end-date" :min="range.start" :max="today" v-model="range.end" @change="$emit('change', range);">
 	</div>
 </template>
 
@@ -14,17 +14,9 @@ export default {
 		return {
 			today: moment().format('YYYY-MM-DD'),
 			range: {
-				start: "",
-				end: ""
+				start: moment().subtract("30","days").format('YYYY-MM-DD'),
+				end: moment().format('YYYY-MM-DD')
 			}
-		}
-	},
-	methods: {
-		changeRange() {
-			if (!this.range.start || !this.range.end) {
-				return;
-			}
-			this.$emit("change", this.range);
 		}
 	}
 }
