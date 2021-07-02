f4b4a023238aa45ed2e1ae39d6378f8a56d14f38
CaveMobster <cavemobster@gmail.com>
2021-07-02 16:55:03 +0200

    Clean up the lurkers and minutes components

--- src/views/Charts/LurkersVsChatters.vue
+++ src/views/Charts/LurkersVsChatters.vue
@@ -6,120 +6,17 @@
 
 <script>
 	import BarChart from '@/components/ChartTypes/BarChart.vue';
+	import ChartFunc from '@/mixins/ChartFunc.vue';
 
 	export default {
 		components: {
 			BarChart
 		},
-		props: {
-			streamData: Array,
-			labels: Array,
-			settings: Object
-		},
+		mixins: [
+			ChartFunc
+		],
 		data: () => ({
 			legendLabels: ["Chatters", "Lurkers"],
-			chartdata: {},
-			options: {},
-			barKey: 0
-		}),
-		beforeMount() {
-			this.options = this.getOptions();
-		},
-		mounted() {
-			this.updateData();
-		},
-		watch: {
-			streamData: function() {
-				this.updateData();
-			}
-		},
-		methods: {
-			updateData() {
-				this.chartdata = this.getChartData();
-				this.barKey++;
-			},
-			getChartData: function() {
-				let data = {
-					labels: this.labels,
-					datasets: [{
-						label: this.legendLabels[0],
-						backgroundColor: "#772ce8",
-						data: this.streamData[0]
-					},
-					{
-						label: this.legendLabels[1],
-						backgroundColor: "#18181b",
-						data: this.streamData[1]
-					}]
-				}
-
-				if (!this.settings.general.shouldHideTrendline) {
-					data.datasets[0].trendlineLinear = {
-						style: "rgba(141,141,141, .8)",
-						lineStyle: "dotted|solid",
-						width: 2
-					}
-				}
-
-				return data;
-			},
-			getOptions: function() {
-				return {
-					responsive: true,
-					maintainAspectRatio: false,
-					legend: {
-						position: "bottom",
-						align: "end",
-						labels: {
-							boxWidth: 13,
-							padding: 30,
-							fontSize: 13
-						}
-					},
-					tooltips: {
-						mode: "x",
-						callbacks: {
-							label: function(tooltipItem, data) {
-								var label = data.datasets[tooltipItem.datasetIndex].label || '';
-
-								if (label) {
-									label += ': ';
-								}
-
-								label += (tooltipItem.yLabel % 1 !== 0 ? tooltipItem.yLabel.toFixed(2) : tooltipItem.yLabel);
-								return label;
-							},
-							title: function(tooltipItems, data) {
-								return data.labels[tooltipItems[0].index];
-							},
-							footer: function(tooltipItems) {
-								let total = 0;
-								if (tooltipItems[1].yLabel === 0) {
-									return 'Total unique viewers: 0';
-								}
-
-								for (let i = 0; i < tooltipItems.length; i++) {
-									total += parseFloat(tooltipItems[i].yLabel);
-								}
-								
-								return 'Total unique viewers: ' + total;
-							}
-						}
-					},
-					scales: {
-						xAxes: [{
-							stacked: true,
-							ticks: {
-								autoSkip: true,
-								autoSkipPadding: 5
-							}
-						}],
-						yAxes: [{
-							stacked: true
-						}]
-					}
-				};
-			}
-		}
+		})
 	}
 </script>
--- src/views/Charts/MinutesPerViewer.vue
+++ src/views/Charts/MinutesPerViewer.vue
@@ -6,102 +6,17 @@
 
 <script>
 	import BarChart from '@/components/ChartTypes/BarChart.vue';
+	import ChartFunc from '@/mixins/ChartFunc.vue';
 
 	export default {
 		components: {
 			BarChart
 		},
-		props: {
-			streamData: Array,
-			labels: Array,
-			settings: Object
-		},
+		mixins: [
+			ChartFunc
+		],
 		data: () => ({
 			legendLabels: ["Minutes per viewer"],
-			chartdata: {},
-			options: {},
-			barKey: 0
-		}),
-		beforeMount() {
-			this.options = this.getOptions();
-		},
-		mounted() {
-			this.updateData();
-		},
-		watch: {
-			streamData: function() {
-				this.updateData();
-			}
-		},
-		methods: {
-			updateData() {
-				this.chartdata = this.getChartData();
-				this.barKey++;
-			},
-			getChartData: function() {
-				let data = {
-					labels: this.labels,
-					datasets: [{
-						label: this.legendLabels[0],
-						backgroundColor: "#772ce8",
-						data: this.streamData[0]
-					}]
-				}
-
-				if (!this.settings.general.shouldHideTrendline) {
-					data.datasets[0].trendlineLinear = {
-						style: "rgba(141,141,141, .8)",
-						lineStyle: "dotted|solid",
-						width: 2
-					}
-				}
-
-				return data;
-			},
-			getOptions: function() {
-				return {
-					responsive: true,
-					maintainAspectRatio: false,
-					legend: {
-						position: "bottom",
-						align: "end",
-						labels: {
-							boxWidth: 13,
-							padding: 30,
-							fontSize: 13
-						}
-					},
-					tooltips: {
-						mode: "x",
-						callbacks: {
-							label: function(tooltipItem, data) {
-								var label = data.datasets[tooltipItem.datasetIndex].label || '';
-
-								if (label) {
-									label += ': ';
-								}
-
-								label += (tooltipItem.yLabel % 1 !== 0 ? tooltipItem.yLabel.toFixed(2) : tooltipItem.yLabel);
-								return label;
-							}
-						}
-					},
-					scales: {
-						xAxes: [{
-							ticks: {
-								autoSkip: true,
-								autoSkipPadding: 5
-							}
-						}],
-						yAxes: [{
-							ticks: {
-								min: 0
-							}
-							
-						}]
-					}
-				};
-			}
-		}
+		})
 	}
 </script>
