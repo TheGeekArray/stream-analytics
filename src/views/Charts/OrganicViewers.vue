9ce205392a22b03c51ab5dfbed989e06ff6aebeb
CaveMobster <cavemobster@gmail.com>
2021-07-02 16:58:50 +0200

    Update the organic viewers component

--- src/views/Charts/OrganicViewers.vue
+++ src/views/Charts/OrganicViewers.vue
@@ -18,127 +18,38 @@
 
 <script>
 	import BarChart from '@/components/ChartTypes/BarChart.vue';
+	import ChartFunc from '@/mixins/ChartFunc.vue';
 
 	export default {
 		components: {
 			BarChart
 		},
-		props: {
-			streamData: Array,
-			labels: Array,
-			settings: Object,
-			topics: Array
-		},
+		mixins: [
+			ChartFunc
+		],
 		data: () => ({
 			rangeTotal: "",
 			rangeOrganicTotal: "",
-			legendLabels: ["Organic", "Hosts/raids/embeds"],
-			chartdata: {},
-			options: {},
-			barKey: 0
+			legendLabels: ["Organic", "Hosts/raids/embeds"]
 		}),
-		beforeMount() {
-			this.options = this.getOptions();
-		},
 		mounted() {
-			this.updateData();
-		},
-		watch: {
-			streamData: function() {
-				this.updateData();
+			if (this.streamData > 1) {
+				this.updateRangeTotals();
 			}
 		},
+		updated() {
+			this.updateRangeTotals();
+		},
 		methods: {
-			updateData() {
+			updateRangeTotals() {
 				this.rangeTotal = this.getRangeTotal(this.streamData[0].map((item, index) => item + this.streamData[1][index])).toFixed(2);
 				this.rangeOrganicTotal = this.getRangeTotal(this.streamData[0]).toFixed(2);
-
-				this.chartdata = this.getChartData();
-				this.barKey++;
 			},
 			getRangeTotal: function(data) {
 				const total = data.reduce((previousValue, currentValue) => previousValue + currentValue);
 
 				return total / data.filter(value => value !== 0).length;
 			},
-			getChartData: function() {
-				let data = {
-					labels: this.labels,
-					datasets: [{
-						label: this.legendLabels[0],
-						backgroundColor: "#772ce8",
-						data: this.streamData[0],
-						
-					},
-					{
-						label: this.legendLabels[1],
-						backgroundColor: "#18181b",
-						data: this.streamData[1]
-					}]
-				}
-
-				if (!this.settings.general.shouldHideTrendline) {
-					data.datasets[0].trendlineLinear = {
-						style: "rgba(141,141,141, .8)",
-						lineStyle: "dotted|solid",
-						width: 2
-					}
-				}
-
-				return data;
-			},
-			getOptions: function() {
-				return {
-					responsive: true,
-					maintainAspectRatio: false,
-					legend: {
-						position: "bottom",
-						align: "end",
-						labels: {
-							boxWidth: 13,
-							padding: 30,
-							fontSize: 13
-						}
-					},
-					tooltips: {
-						mode: "x",
-						callbacks: {
-							label: function(tooltipItem, data) {
-								var label = data.datasets[tooltipItem.datasetIndex].label || '';
-
-								if (label) {
-									label += ': ';
-								}
-
-								label += (tooltipItem.yLabel % 1 !== 0 ? tooltipItem.yLabel.toFixed(2) : tooltipItem.yLabel);
-								return label;
-							},
-							title: function(tooltipItems, data) {
-								return data.labels[tooltipItems[0].index];
-							},
-							footer: function(tooltipItems) {
-								let total = 0;
-								for (let i = 0; i < tooltipItems.length; i++) {
-									total += parseFloat(tooltipItems[i].yLabel);
-								}
-								return 'Total: ' + total.toFixed(2);
-							}
-						}
-					},
-					scales: {
-						xAxes: [{
-							stacked: true,
-							ticks: {
-								autoSkip: true,
-								autoSkipPadding: 5
-							}
-						}],
-						yAxes: [{
-							stacked: true
-						}]
-					}
-				};
-			}
 		}
 	}
 </script>
